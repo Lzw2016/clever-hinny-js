0142674f5a6b96147beaf7f4b4aaa695e98e52f2
lizw <lzw1000000@163.com>
2020-07-30 13:04:11 +0800

    微调

--- test/src/data-jdbc-test.ts
+++ test/src/data-jdbc-test.ts
@@ -1,11 +1,15 @@
 import { jdbcDatabase } from "@hinny/data-jdbc";
 
+const log = LoggerFactory.getLogger("/test/src/data-jdbc-test.ts");
+
 // console.log("jdbcDatabase -> ", jdbcDatabase.getClass());
 const jdbc = jdbcDatabase.getDefault();
 
+
 const t01 = function () {
     const resList = jdbc.queryList("select * from `clever-template`.tb_order_main limit 3");
-    console.log("resList -> ", resList[0]);
+    log.info("resList -> {}", JSON.stringify(resList));
+    console.log("resList -> ", resList[1]);
     // console.log("order_id -> ", resList[0].get("order_id"));
 }
 
