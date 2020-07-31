4e84997378ea0c5f9be8d9293a0e5d9bb4b24c10
lizw <1183409807@qq.com>
2020-07-31 20:18:40 +0800

    add BaseTypes.d.ts

--- types/java/BaseTypes.d.ts
+++ types/java/BaseTypes.d.ts
@@ -1,33 +1,128 @@
-type JByte = number | never;
+/**
+ * Java 基本类型 byte (-128~127)
+ */
+type JByte = number;
 
-type JShort = number | never;
+/**
+ * Java 基本类型 short (-32,768 ~ 32,767)
+ */
+type JShort = number;
 
-type JInt = number | never;
+/**
+ * Java 基本类型 int (-2,147,483,648 ~ 2,147,483,647)
+ */
+type JInt = number;
 
-type JLong = number | never;
+/**
+ * Java 基本类型 long (-9,223,372,036,854,774,808 ~ 9,223,372,036,854,774,807)
+ */
+type JLong = number;
 
-type JDouble = number | never;
+/**
+ * Java 基本类型 double
+ */
+type JDouble = number;
 
-type JBoolean = boolean | never;
+/**
+ * Java 基本类型 boolean
+ */
+type JBoolean = boolean;
 
-type JChar = string | never;
+/**
+ * Java 基本类型 char
+ */
+type JChar = string;
 
-type JString = string | never;
+/**
+ * Java 基本类型 String
+ */
+type JString = string;
 
-type JDate = never;
+/**
+ * java.util.Date 类型，不要在JavaScript/TypeScript环境中创建该类型对象<br />
+ * 请使用内置对象“JObject”创建该类型实例<br />
+ * @see JObject
+ */
+interface JDate {
+    java_util_Date: "java.util.Date";
+}
 
-type BigDecimal = never;
+/**
+ * java.math.BigDecimal 类型，不要在JavaScript/TypeScript环境中创建该类型对象<br />
+ * 请使用内置对象“JObject”创建该类型实例<br />
+ * @see JObject
+ */
+interface JBigDecimal {
+    java_math_BigDecimal: "java.math.BigDecimal";
+}
 
 interface JObject {
+    /**
+     * 创建Java byte 值
+     */
     asJByte(arg: number | string): JByte;
 
+    /**
+     * 创建Java short 值
+     */
     asJShort(arg: number | string): JShort;
 
+    /**
+     * 创建Java int 值
+     */
     asJInt(arg: number | string): JInt;
 
-    as(): JByte;
+    /**
+     * 创建Java long 值
+     */
+    asJLong(arg: number | string): JLong;
 
-    // TODO: ...
+    /**
+     * 创建Java double 值
+     */
+    asJDouble(arg: number | string): JDouble;
+
+    /**
+     * 创建Java boolean 值
+     * <pre>
+     *     false    -> false
+     *     true     -> true
+     *     "false"  -> false
+     *     "FALSE"  -> false
+     *     "FALse"  -> false
+     *     "true"   -> true
+     *     "TRUE"   -> true
+     *     "TRue"   -> true
+     * </pre>
+     */
+    asJBoolean(arg: boolean | string): JBoolean;
+
+    /**
+     * 创建Java char 值
+     */
+    asJChar(arg: string): JChar;
+
+    /**
+     * 创建Java String 值
+     */
+    asJString(arg: any): JString;
+
+    /**
+     * 创建Java java.util.Date 对象,支持以下格式<br />
+     * 1. <br />
+     * 2. <br />
+     * 3. <br />
+     * 4. <br />
+     * 5. <br />
+     */
+    asJDate(arg: string): JDate;
+
+    /**
+     * 创建Java java.math.BigDecimal 对象
+     */
+    asJBigDecimal(arg: string): JBigDecimal;
+
+    // TODO 补充常用类型
 }
 
 declare const JObject: JObject;
