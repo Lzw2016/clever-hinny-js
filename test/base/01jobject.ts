68ff4d87a927e979d139ad2bf9bd1ee704c243d1
lizw <1183409807@qq.com>
2020-08-01 22:34:12 +0800

    微调

--- test/base/01jobject.ts
+++ test/base/01jobject.ts
@@ -27,7 +27,32 @@ const t02 = function () {
     log.info("# asJBigDecimal   -> {}", JObject.asJBigDecimal('1354741344987654323456765434567564564568989.564948989745189789454894894864'));
 }
 
+const t03 = function () {
+    log.info("# asJList         -> {}", JObject.asJList("111", "222", "333"));
+    log.info("# asJList         -> {}", JObject.asJList(["111", "222", "333"]));
+    log.info("# asJList         -> {}", JObject.asJList([111, 222, 333]));
+    log.info("# asJSet          -> {}", JObject.asJSet("111", "222", "333"));
+    log.info("# asJSet          -> {}", JObject.asJSet(["111", "222", "333"]));
+    log.info("# asJSet          -> {}", JObject.asJSet([111, 222, 333]));
+    log.info("# asJMap          -> {}", JObject.asJMap({
+        a: "aaa", b: 123, c: false, d: 12.87, fuc: function (a: number, b: number) {
+            return a + b;
+        }
+    }));
+    log.info("# asJMap          -> {}", JObject.asJMap([111, 222, 333]));
+}
+
+const t04 = function () {
+    const list = JObject.asJList("111", "222", "333") as any as string[];
+    log.info(" -> {}", list.map(value => `${value}|AAA`)); // 调用失败，没有map函数
+
+    // const array = ["111", "222", "333"];
+    // console.info(" -> ", array.map(value => `${value}|AAA`));
+}
+
 export {
     t01,
     t02,
+    t03,
+    t04,
 }
--- test/data-jdbc/data-jdbc-test.ts
+++ test/data-jdbc/data-jdbc-test.ts
@@ -3,7 +3,12 @@ import {AnyEntity, jdbcDatabase} from "@hinny/data-jdbc";
 const log = LoggerFactory.getLogger(__filename);
 
 interface Entity extends AnyEntity {
-    order_id: JString;
+    order_id: JInt;
+    user_agent_id: JInt;
+    store_no: JString;
+    total_price: JBigDecimal;
+
+    // order_id: 1, user_agent_id: 1, site_id: 2, store_id: 3, store_no: "4", cust_id: 5,
 }
 
 // console.log("jdbcDatabase -> ", jdbcDatabase.getClass());
@@ -12,10 +17,9 @@ const jdbc = jdbcDatabase.getDefault<Entity>();
 const t01 = function () {
     log.info("DbType -> {}", jdbc.getDbType());
     const resList = jdbc.queryList("select * from `clever-template`.tb_order_main limit 3");
-    log.info("resList -> {}", resList);
-    // @ts-ignore
-    log.info("order_id -> {}", resList[1].get("order_id"));
-    // log.info("order_id -> {}", resList[1].order_id);
+    // log.info("resList -> {}", [resList]);
+    // log.info("order_id -> {}", resList[1].get("order_id"));
+    log.info("order_id -> {}", resList[1].total_price);
     // const b = resList[1].b;
     // const d = resList[1].d;
     // console.log("### -> ", b, d);
