8628dbc529ddb3178f0520e8654fc1dde297bbd0
lizw <1183409807@qq.com>
2020-08-02 23:24:21 +0800

    微调

--- test/data-jdbc/data-jdbc-test.ts
+++ test/data-jdbc/data-jdbc-test.ts
@@ -11,28 +11,25 @@ interface Entity extends AnyEntity {
     // order_id: 1, user_agent_id: 1, site_id: 2, store_id: 3, store_no: "4", cust_id: 5,
 }
 
-// console.log("jdbcDatabase -> ", jdbcDatabase.getClass());
 const jdbc = jdbcDatabase.getDefault();
 
 const t01 = function () {
     log.info("DbType -> {}", jdbc.getDbType());
     const resList = jdbc.queryList<Entity>("select * from `clever-template`.tb_order_main limit 3");
-    log.info("resList           -> {}", resList);
-    log.info("resList           -> {}", resList[0]);
-    log.info("fromJMap          -> {}", Interop.fromJMap(resList[1]));
-    log.info("order_id          -> {}", Interop.fromJMap(resList[1]).order_id);
-    log.info("total_price       -> {}", Interop.fromJMap(resList[1]).total_price);
+    log.info("resList                           -> {}", resList);
+    log.info("[resList]                         -> {}", [resList]);
+    log.info("resList[0]                        -> {}", resList[0]);
+    log.info("fromJMap(resList[1])              -> {}", Interop.fromJMap(resList[1]));
+    log.info("resList[1].order_id               -> {}", resList[1].order_id);
+    log.info("fromJMap(resList[1]).order_id     -> {}", Interop.fromJMap(resList[1]).order_id);
+    log.info("fromJMap(resList[1]).total_price  -> {}", Interop.fromJMap(resList[1]).total_price);
+    log.info("undefined                         -> {}", undefined);
 
-    // log.info("map               -> {}", Interop.fromJList(resList)[0]);
     // log.info("total_price       -> {}", Interop.fromJMap({}));
     // log.info("order_id          -> {}", resList[1].get("order_id"));
-    // log.info("undefined         -> {}", undefined);
-
 }
 
 
 export {
     t01,
 }
-
-// exports.t01 = t01;
