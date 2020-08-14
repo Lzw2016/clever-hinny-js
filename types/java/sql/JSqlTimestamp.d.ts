183dc609a2231c58203064612fbf3991b8f2de57
ymx <mlrsyz@vip.qq.com>
2020-08-14 17:31:06 +0800

    微调

--- types/java/sql/JSqlTimestamp.d.ts
+++ types/java/sql/JSqlTimestamp.d.ts
@@ -7,6 +7,25 @@ interface JSqlTimestamp extends JDate {
     java_sql_Timestamp: "java.sql.Timestamp";
 
     /**
+     * 指示此 Timestamp 对象是否晚于给定的 Timestamp 对象
+     */
+    after(ts: JSqlTimestamp): JBoolean;
+
+    /**
+     * 指示此 Timestamp 对象是否早于给定的 Timestamp 对象
+     */
+    before(ts: JSqlTimestamp): JBoolean;
+
+    /**
+     * 将此 Timestamp 对象与给定的 Date（必须为 Timestamp 对象）相比较
+     * 将此 Timestamp 对象与给定的 Date（必须为 Timestamp 对象）相比较 <br>
+     * this 早于 参数o 返回-1  <br>
+     * this 晚于 参数o 返回 1  <br>
+     * 时间相等返回0  <br>
+     */
+    compareTo(o: JSqlTimestamp): JBoolean;
+
+    /**
      * 获取此 Timestamp 对象的 nanos 值
      */
     getNanos(): JInt;
