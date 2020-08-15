1f33f30039bfbd196acb3966dffbf0369cde2e8a
lizw <1183409807@qq.com>
2020-08-15 20:02:25 +0800

    新增文件 AssertUtils.ts CommonUtils.ts CookieUtils.ts CryptoUtils.ts DataSizeUtils.ts DateUtils.ts DigestUtils.ts EncodeDecodeUtils.ts ExcelUtils.ts HttpRequestUtils.ts HttpUtils.ts IdCardUtils.ts IDGenerateUtils.ts ImageValidateUtils.ts IPAddressUtils.ts JsonUtils.ts PinyinUtils.ts RMBUtils.ts SendMailUtils.ts SpringContext.ts StringUtils.ts ThreadUtils.ts TreeUtils.ts ValidatorUtils.ts XmlUtils.ts ZxingUtils.ts

--- core/AssertUtils.ts
+++ core/AssertUtils.ts
@@ -1,3 +1,23 @@
 export interface AssertUtils {
+    /**
+     * 断言表达式的值是 true
+     */
+    isTrue(expression: JBoolean, message: JString): void;
 
+
+
+
+
+
+
+
+
+}
+
+
+const assertUtils: AssertUtils = Java.type('org.clever.hinny.core.AssertUtils').Instance;
+
+
+export {
+    assertUtils,
 }
