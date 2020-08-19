72d6724d87e53d8b7d841c155b760c0ec3e26239
lizw <lzw1000000@163.com>
2020-08-19 18:31:54 +0800

    ExcelUtils.ts 40%

--- core/ExcelUtils.ts
+++ core/ExcelUtils.ts
@@ -60,6 +60,165 @@ export enum ExcelLocale {
     TRADITIONAL_CHINESE = "TRADITIONAL_CHINESE",
 }
 
+export enum BuiltinFormats {
+// 0, "General"
+// 1, "0"
+// 2, "0.00"
+// 3, "#,##0"
+// 4, "#,##0.00"
+// 5, "$#,##0_);($#,##0)"
+// 6, "$#,##0_);[Red]($#,##0)"
+// 7, "$#,##0.00);($#,##0.00)"
+// 8, "$#,##0.00_);[Red]($#,##0.00)"
+// 9, "0%"
+// 0xa, "0.00%"
+// 0xb, "0.00E+00"
+// 0xc, "# ?/?"
+// 0xd, "# ??/??"
+// 0xe, "m/d/yy"
+// 0xf, "d-mmm-yy"
+// 0x10, "d-mmm"
+// 0x11, "mmm-yy"
+// 0x12, "h:mm AM/PM"
+// 0x13, "h:mm:ss AM/PM"
+// 0x14, "h:mm"
+// 0x15, "h:mm:ss"
+// 0x16, "m/d/yy h:mm"
+// // 0x17 - 0x24 reserved for international and undocume
+// 0x25, "#,##0_);(#,##0)"
+// 0x26, "#,##0_);[Red](#,##0)"
+// 0x27, "#,##0.00_);(#,##0.00)"
+// 0x28, "#,##0.00_);[Red](#,##0.00)"
+// 0x29, "_(* #,##0_);_(* (#,##0);_(* \"-\"_);_(@_)"
+// 0x2a, "_($* #,##0_);_($* (#,##0);_($* \"-\"_);_(@_)"
+// 0x2b, "_(* #,##0.00_);_(* (#,##0.00);_(* \"-\"??_);_(@_)"
+// 0x2c, "_($* #,##0.00_);_($* (#,##0.00);_($* \"-\"??_);_(@_)"
+// 0x2d, "mm:ss"
+// 0x2e, "[h]:mm:ss"
+// 0x2f, "mm:ss.0"
+// 0x30, "##0.0E+0"
+// 0x31, "@" - This is text format.
+// 0x31  "text" - Alias for "@"
+}
+
+export enum HorizontalAlignment {
+    GENERAL = "GENERAL",
+    LEFT = "LEFT",
+    CENTER = "CENTER",
+    RIGHT = "RIGHT",
+    FILL = "FILL",
+    JUSTIFY = "JUSTIFY",
+    CENTER_SELECTION = "CENTER_SELECTION",
+    DISTRIBUTED = "DISTRIBUTED",
+}
+
+export enum VerticalAlignment {
+    TOP = "TOP",
+    CENTER = "CENTER",
+    BOTTOM = "BOTTOM",
+    JUSTIFY = "JUSTIFY",
+    DISTRIBUTED = "DISTRIBUTED",
+}
+
+export enum BorderStyle {
+    NONE = (0x0),
+    THIN = (0x1),
+    MEDIUM = (0x2),
+    DASHED = (0x3),
+    DOTTED = (0x4),
+    THICK = (0x5),
+    DOUBLE = (0x6),
+    HAIR = (0x7),
+    MEDIUM_DASHED = (0x8),
+    DASH_DOT = (0x9),
+    MEDIUM_DASH_DOT = (0xA),
+    DASH_DOT_DOT = (0xB),
+    MEDIUM_DASH_DOT_DOT = (0xC),
+    SLANTED_DASH_DOT = (0xD),
+}
+
+export enum IndexedColors {
+    BLACK1 = (0),
+    WHITE1 = (1),
+    RED1 = (2),
+    BRIGHT_GREEN1 = (3),
+    BLUE1 = (4),
+    YELLOW1 = (5),
+    PINK1 = (6),
+    TURQUOISE1 = (7),
+    BLACK = (8),
+    WHITE = (9),
+    RED = (10),
+    BRIGHT_GREEN = (11),
+    BLUE = (12),
+    YELLOW = (13),
+    PINK = (14),
+    TURQUOISE = (15),
+    DARK_RED = (16),
+    GREEN = (17),
+    DARK_BLUE = (18),
+    DARK_YELLOW = (19),
+    VIOLET = (20),
+    TEAL = (21),
+    GREY_25_PERCENT = (22),
+    GREY_50_PERCENT = (23),
+    CORNFLOWER_BLUE = (24),
+    MAROON = (25),
+    LEMON_CHIFFON = (26),
+    LIGHT_TURQUOISE1 = (27),
+    ORCHID = (28),
+    CORAL = (29),
+    ROYAL_BLUE = (30),
+    LIGHT_CORNFLOWER_BLUE = (31),
+    SKY_BLUE = (40),
+    LIGHT_TURQUOISE = (41),
+    LIGHT_GREEN = (42),
+    LIGHT_YELLOW = (43),
+    PALE_BLUE = (44),
+    ROSE = (45),
+    LAVENDER = (46),
+    TAN = (47),
+    LIGHT_BLUE = (48),
+    AQUA = (49),
+    LIME = (50),
+    GOLD = (51),
+    LIGHT_ORANGE = (52),
+    ORANGE = (53),
+    BLUE_GREY = (54),
+    GREY_40_PERCENT = (55),
+    DARK_TEAL = (56),
+    SEA_GREEN = (57),
+    DARK_GREEN = (58),
+    OLIVE_GREEN = (59),
+    BROWN = (60),
+    PLUM = (61),
+    INDIGO = (62),
+    GREY_80_PERCENT = (63),
+    AUTOMATIC = (64),
+}
+
+export enum FillPatternType {
+    NO_FILL = (0),
+    SOLID_FOREGROUND = (1),
+    FINE_DOTS = (2),
+    ALT_BARS = (3),
+    SPARSE_DOTS = (4),
+    THICK_HORZ_BANDS = (5),
+    THICK_VERT_BANDS = (6),
+    THICK_BACKWARD_DIAG = (7),
+    THICK_FORWARD_DIAG = (8),
+    BIG_SPOTS = (9),
+    BRICKS = (10),
+    THIN_HORZ_BANDS = (11),
+    THIN_VERT_BANDS = (12),
+    THIN_BACKWARD_DIAG = (13),
+    THIN_FORWARD_DIAG = (14),
+    SQUARES = (15),
+    DIAMONDS = (16),
+    LESS_DOTS = (17),
+    LEAST_DOTS = (18),
+}
+
 export interface AbstractParameterBuilder<T extends AbstractParameterBuilder<T>> {
     /** 如果日期使用1904窗口，则为True；如果使用1900日期窗口，则为false */
     use1904windowing(use1904windowing: boolean): T;
@@ -302,6 +461,7 @@ export interface ContentFontStyle {
     bold?: boolean;
 }
 
+/** 合并区域配置 */
 export interface ContentLoopMerge {
     /** 行 */
     eachRow: number;
@@ -310,83 +470,169 @@ export interface ContentLoopMerge {
     columnExtend: number;
 }
 
+/** 行高 */
 export interface ContentRowHeight {
     /** 行高 */
     rowHeight: number;
 }
 
 export interface ContentStyle {
-    // dataFormat: ;
-    // hidden: ;
-    // locked: ;
-    // quotePrefix: ;
-    // horizontalAlignment: ;
-    // verticalAlignment: ;
-    // rotation: ;
-    // indent: ;
-    // borderLeft: ;
-    // borderRight: ;
-    // borderTop: ;
-    // borderBottom: ;
-    // leftBorderColor: ;
-    // rightBorderColor: ;
-    // topBorderColor: ;
-    // bottomBorderColor: ;
-    // fillPatternType: ;
-    // fillBackgroundColor: ;
-    // fillForegroundColor: ;
-    // shrinkToFit: ;
+    /** 设置数据格式（必须是有效格式）。内置格式在内置信息中定义 */
+    dataFormat?: BuiltinFormats;
+    /** 将单元格使用此样式设置为隐藏 */
+    hidden?: JBoolean;
+    /** 将单元格使用此样式设置为锁定 */
+    locked?: JBoolean;
+    /**
+     * 打开或关闭样式的“Quote Prefix”或“123 Prefix”，
+     * 用于告诉Excel，看起来像数字或公式的内容不应被视为打开。
+     * 打开此选项有点（但不是完全打开，请参见IgnoredErrorType）类似于在Excel中为单元格值添加前缀
+     */
+    quotePrefix?: JBoolean;
+    /** 设置单元格的水平对齐方式 */
+    horizontalAlignment?: HorizontalAlignment;
+    /** 设置是否应该换行。将此标志设置为true可以通过在多行上显示所有内容来使其在一个单元格中可见 */
+    wrapped?: JBoolean;
+    /** 设置单元格的垂直对齐方式 */
+    verticalAlignment?: VerticalAlignment;
+    /**
+     * 设置单元格中文本的旋转度<br />
+     * 注意：HSSF使用-90至90度的值，而XSSF使用0至180度的值。
+     * 此方法的实现将在这两个值范围之间进行映射，
+     * 但是，相应的getter返回此CellStyle所应用的当前Excel文件格式类型所要求的范围内的值。
+     */
+    rotation?: JShort;
+    /** 设置空格数以缩进单元格中的文本 */
+    indent?: JShort;
+    /** 设置要用于单元格左边框的边框类型 */
+    borderLeft?: BorderStyle;
+    /** 设置用于单元格右边框的边框类型 */
+    borderRight?: BorderStyle;
+    /** 设置要用于单元格顶部边框的边框类型 */
+    borderTop?: BorderStyle;
+    /** 设置用于单元格底部边框的边框类型 */
+    borderBottom?: BorderStyle;
+    /**  设置用于左边框的颜色*/
+    leftBorderColor?: IndexedColors;
+    /** 设置用于右边框的颜色 */
+    rightBorderColor?: IndexedColors;
+    /** 设置要用于顶部边框的颜色 */
+    topBorderColor?: IndexedColors;
+    /** 设置用于底边框的颜色 */
+    bottomBorderColor?: IndexedColors;
+    /** 设置为1会使单元格充满前景色...不知道其他值 */
+    fillPatternType: FillPatternType;
+    /**  设置背景填充颜色*/
+    fillBackgroundColor?: IndexedColors;
+    /**
+     * 设置前景色填充颜色<br />
+     * 注意：确保将前景色设置为背景颜色之前
+     */
+    fillForegroundColor?: IndexedColors;
+    /** 控制如果文本太长，是否应自动调整单元格的大小以缩小以适合 */
+    shrinkToFit?: JBoolean;
 }
 
 export interface HeadFontStyle {
-    // fontName: ;
-    // fontHeightInPoints: ;
-    // italic: ;
-    // strikeout: ;
-    // color: ;
-    // typeOffset: ;
-    // underline: ;
-    // charset: ;
-    // bold: ;
+    /** 字体的名称（如: Arial） */
+    fontName?: string;
+
+    /** 以熟悉的测量单位表示的高度- points */
+    fontHeightInPoints?: number;
+
+    /** 是否使用斜体 */
+    italic?: boolean;
+
+    /** 是否在文本中使用删除线水平线 */
+    strikeout?: boolean;
+
+    /** 字体的颜色 */
+    color?: number;
+
+    /** 设置normal、super或subscript */
+    typeOffset?: number;
+
+    /** 要使用的文本下划线 */
+    underline?: number;
+
+    /** 设置要使用的字符集 */
+    charset?: number;
+
+    /** 粗体 */
+    bold?: boolean;
 }
 
 export interface HeadRowHeight {
     /** 表格头行高 */
-    headRowHeight: number;
+    headRowHeight?: number;
 }
 
 export interface HeadStyle {
-    // dataFormat: ;
-    // hidden: ;
-    // locked: ;
-    // quotePrefix: ;
-    // horizontalAlignment: ;
-    // verticalAlignment: ;
-    // rotation: ;
-    // indent: ;
-    // borderLeft: ;
-    // borderRight: ;
-    // borderTop: ;
-    // borderBottom: ;
-    // leftBorderColor: ;
-    // rightBorderColor: ;
-    // topBorderColor: ;
-    // bottomBorderColor: ;
-    // fillPatternType: ;
-    // fillBackgroundColor: ;
-    // fillForegroundColor: ;
-    // shrinkToFit: ;
+    /** 设置数据格式（必须是有效格式）。内置格式在内置信息中定义 */
+    dataFormat?: BuiltinFormats;
+    /** 将单元格使用此样式设置为隐藏 */
+    hidden?: JBoolean;
+    /** 将单元格使用此样式设置为锁定 */
+    locked?: JBoolean;
+    /**
+     * 打开或关闭样式的“Quote Prefix”或“123 Prefix”，
+     * 用于告诉Excel，看起来像数字或公式的内容不应被视为打开。
+     * 打开此选项有点（但不是完全打开，请参见IgnoredErrorType）类似于在Excel中为单元格值添加前缀
+     */
+    quotePrefix?: JBoolean;
+    /** 设置单元格的水平对齐方式 */
+    horizontalAlignment?: HorizontalAlignment;
+    /** 设置是否应该换行。将此标志设置为true可以通过在多行上显示所有内容来使其在一个单元格中可见 */
+    wrapped?: JBoolean;
+    /** 设置单元格的垂直对齐方式 */
+    verticalAlignment?: VerticalAlignment;
+    /**
+     * 设置单元格中文本的旋转度<br />
+     * 注意：HSSF使用-90至90度的值，而XSSF使用0至180度的值。
+     * 此方法的实现将在这两个值范围之间进行映射，
+     * 但是，相应的getter返回此CellStyle所应用的当前Excel文件格式类型所要求的范围内的值。
+     */
+    rotation?: JShort;
+    /** 设置空格数以缩进单元格中的文本 */
+    indent?: JShort;
+    /** 设置要用于单元格左边框的边框类型 */
+    borderLeft?: BorderStyle;
+    /** 设置用于单元格右边框的边框类型 */
+    borderRight?: BorderStyle;
+    /** 设置要用于单元格顶部边框的边框类型 */
+    borderTop?: BorderStyle;
+    /** 设置用于单元格底部边框的边框类型 */
+    borderBottom?: BorderStyle;
+    /**  设置用于左边框的颜色*/
+    leftBorderColor?: IndexedColors;
+    /** 设置用于右边框的颜色 */
+    rightBorderColor?: IndexedColors;
+    /** 设置要用于顶部边框的颜色 */
+    topBorderColor?: IndexedColors;
+    /** 设置用于底边框的颜色 */
+    bottomBorderColor?: IndexedColors;
+    /** 设置为1会使单元格充满前景色...不知道其他值 */
+    fillPatternType: FillPatternType;
+    /**  设置背景填充颜色*/
+    fillBackgroundColor?: IndexedColors;
+    /**
+     * 设置前景色填充颜色<br />
+     * 注意：确保将前景色设置为背景颜色之前
+     */
+    fillForegroundColor?: IndexedColors;
+    /** 控制如果文本太长，是否应自动调整单元格的大小以缩小以适合 */
+    shrinkToFit?: JBoolean;
 }
 
 export interface OnceAbsoluteMerge {
-    /**  */
-    firstRowIndex: number;
-    /**  */
-    lastRowIndex: number;
-    /**  */
-    firstColumnIndex: number;
-    /**  */
-    lastColumnIndex: number;
+    /** 第一行 */
+    firstRowIndex: JInt;
+    /** 最后一行 */
+    lastRowIndex: JInt;
+    /** 第一列 */
+    firstColumnIndex: JInt;
+    /** 最后一列 */
+    lastColumnIndex: JInt;
 }
 
 export interface ExcelHead {
@@ -606,6 +852,29 @@ export interface ExcelReaderHeadConfig extends ExcelProperty, Partial<DateTimeFo
     // TODO 读取数据校验配置
 }
 
+/** 写入Excel时的表头配置 */
+export interface ExcelWriterHeadConfig
+    extends ExcelProperty,
+        Partial<DateTimeFormat>,
+        Partial<NumberFormat>,
+        Partial<ColumnWidth>,
+        Partial<ContentFontStyle>,
+        Partial<ContentLoopMerge>,
+        Partial<ContentStyle>,
+        Partial<HeadFontStyle>,
+        Partial<HeadStyle> {
+}
+
+export interface ExcelWriterStyleConfig
+    extends Partial<ContentFontStyle>,
+        Partial<ContentRowHeight>,
+        Partial<ContentStyle>,
+        Partial<HeadFontStyle>,
+        Partial<HeadRowHeight>,
+        Partial<HeadStyle>,
+        Partial<OnceAbsoluteMerge> {
+}
+
 /** 读取Excel时的初始化配置 */
 export class ExcelReaderConfig<T extends object> {
     /** Excel文件名称(或者Excel文件路径) */
@@ -742,8 +1011,12 @@ export class ExcelWriterConfig<T extends object> {
     autoTrim?: JBoolean = true;
     /** Excel表头 */
     columns?: {
-        [column in keyof T]: ExcelReaderHeadConfig;
+        [column in keyof T]: ExcelWriterHeadConfig;
     };
+
+    // ----------------------------------------------------------------------
+    /** 全局样式配置 */
+    styleConfig?: ExcelWriterStyleConfig;
 }
 
 export interface ExcelUtils {
