835aef31210cb61bd356dd382980e56d7ab2f839
ymx <mlrsyz@vip.qq.com>
2020-08-25 16:33:01 +0800

    bug测试

--- test/core/23ThreadUtils.ts
+++ test/core/23ThreadUtils.ts
@@ -3,9 +3,9 @@ import {threadUtils} from '@hinny/core';
 const log = LoggerFactory.getLogger(__filename);
 
 const t01 = function () {
-    log.info("track     --------------->{}", threadUtils.track(Java.type("java.lang.Thread").currentThread()));
+    // log.info("track     --------------->{}", threadUtils.track(Java.type("java.lang.Thread").currentThread()));
     log.info("track     --------------->{}", threadUtils.track());
-    log.info("printTrack--------------->{}", threadUtils.printTrack(Java.type("java.lang.Thread").currentThread()));
+    // log.info("printTrack--------------->{}", threadUtils.printTrack(Java.type("java.lang.Thread").currentThread()));
     log.info("sleep     --------------->{}", threadUtils.sleep(10000));
     log.info("printTrack--------------->{}", threadUtils.printTrack());
 }
