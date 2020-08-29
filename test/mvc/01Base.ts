666497213ca11e8ee2926166aa24a29cf9474e1a
lizw <1183409807@qq.com>
2020-08-29 15:02:39 +0800

    mvc 15%

--- test/mvc/01Base.ts
+++ test/mvc/01Base.ts
@@ -1,16 +1,31 @@
 // import { commonUtils } from "@hinny/core";
 import { jdbcDatabase } from "@hinny/data-jdbc";
-import { HttpHandle } from "@hinny/mvc";
+import { HttpHandle, HttpMethod, HttpRouter } from "@hinny/mvc";
 
 const log = LoggerFactory.getLogger(__filename);
 const jdbc = jdbcDatabase.getDefault();
 
 const t01: HttpHandle = ctx => {
     const {request} = ctx;
-    log.info("-------------------> {}", request.getHeaderNames());
+    log.info("getHeaderNames --> {}", request.getHeaderNames());
     return jdbc.queryList("select * from tb_order_main limit 16");
 }
 
+const t02: HttpRouter = {
+    method: HttpMethod.POST,
+
+    handle: ctx => {
+        log.info("getHeaderNames -->");
+        return ctx.request.getHeaderNames();
+    },
+
+    get: ctx => {
+        log.info("getParameterNames -->");
+        return ctx.request.getParameterNames();
+    },
+};
+
 export {
     t01,
+    t02,
 }
